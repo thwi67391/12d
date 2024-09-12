475af9004ef07f49b80a1117db272d5d3e8ac74a
thwi67391 <theiswilken@gmail.com>
2024-09-12 11:27:31 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HenningsenLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(string.Format("{0} {1} {2}", i, i + 1, i + 2));
+            }
+        }
+    }
+}
